c913ba686a368a737b68a348cc9c50b2701cf74d
Bill Mills <bill.mills@linaro.org>
2024-03-15 15:40:23 -0400

    Fix pre-build script
    The downloads were being moved but not as I intended.
    Also the script would complain in a fresh project directory.
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-DATE=$(date +%Y-%m-%d-%H-%M-%S)
-D=downloads-${DATE}
-cd build
-mv tmp tmp-${DATE}
-mkdir -p $D
-cd downloads
-find -name "*wmamills*" | xargs -n 1 -I '{}' mv '{}' ./$D/
+echo "pre-build for PWD=$PWD"
+if [ -d build ]; then
+    cd build
+    DATE=$(date +%Y-%m-%d-%H-%M-%S)
+
+    if [ -d tmp ]; then
+        mv tmp tmp-${DATE}
+    fi
+
+    if [ -d downloads ]; then
+        D=downloads-${DATE}
+        mkdir -p $D/git2
+        cd downloads
+        # move whole directories first, then everything else
+        find -type d -name "*wmamills*" | xargs -n 1 -I '{}' mv '{}' ../$D/'{}'
+        find -name "*wmamills*" | xargs -n 1 -I '{}' mv '{}'  ../$D/'{}'
+    fi
+else
+    echo "no build dir"
+fi
