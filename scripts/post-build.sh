3b381fd7140bd01c11fa6cd7951598a4f0bf0ed9
Bill Mills <bill.mills@linaro.org>
2024-03-17 16:54:45 -0400

    post-build: deploy latest build to TFTP dir
    If not on the remote and the DEPLOY_TFTP_DIR variable is defined, then
    automatically deploy the latest build to the TFTP dir.
    
    * update example to show use
    * update tftp example files to show use
    
    Signed-off-by: Bill Mills <bill.mills@linaro.org>

---
+++ scripts/post-build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Nothing to do on remote side of remote build
+if [ x"$CONTEXT" == x"remote" ]; then
+    exit 0
+fi
+
+# if we don't have an FTP deploy dir, then nothing to do
+if [ x"$DEPLOY_TFTP_DIR" == x"" ]; then
+    exit 0
+fi
+
+M=genericarm64
+L=build-${M}-latest
+LINK=$(readlink $L)
+D=$DEPLOY_TFTP_DIR
+
+echo "Deploy $L ($LINK) to $D"
+
+mkdir -p $D
+rm -rf $D/Image*
+rm -rf $D/modules*.tgz
+rm -rf $D/*.rootfs*.tar.gz
+rm -rf $D/kernel-dev*.rpm
+cp -a $L/deploy/images/${M}/Image* $D/
+cp -a $L/deploy/images/${M}/modules*.tgz $D/
+cp -a $L/deploy/images/${M}/*.rootfs*.tar.gz $D/
+cp -a $L/deploy/rpm/${M}/kernel-dev*.rpm $D/
+echo $LINK >$D/info.txt
+make-image-targets $D
+echo "Deploy done $L ($LINK) to $D"
